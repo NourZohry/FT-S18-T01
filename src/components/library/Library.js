ef539a3b17547aa1be336623f6a4197d5d825194
NourZohry <nzohry@gmail.com>
2023-06-10 20:29:02 +0300

    Implement local storage

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,30 +1,35 @@
 import './Library.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Searchbar } from '../searchbar/Searchbar';
 import { SearchResults } from '../searchresults/SearchResults';
 import { Shelf } from '../shelf/Shelf';
 
 export default function Library() {
     const [results, setResults] = useState([]);
-    const [shelfBooks, setShelfBooks] = useState([]);
-
-
-    
     const [showSearch, setShowSearch] = useState(false);
 
-    
+    const [shelfBooks, setShelfBooks] = useState(() => {
+        const localData = localStorage.getItem('shelf-books');
+        return localData ? JSON.parse(localData) : []
+    });
+    useEffect(() => {
+        localStorage.setItem('shelf-books', JSON.stringify(shelfBooks))
+    }, [shelfBooks]);
+
+
 
     return (
         <div className="container">
-            {showSearch ? 
-            <div className="searchbar-container">
-                <Searchbar setResults={setResults} setShowSearch={setShowSearch}/>
-                <SearchResults results={results} setShelfBooks={setShelfBooks} shelfBooks={shelfBooks}/>  
-            </div>
-            :
-            <div className="shelf">
-                <Shelf setShelfBooks={setShelfBooks} shelfBooks={shelfBooks} setShowSearch={setShowSearch}/>
-            </div>
+            {showSearch &&
+                <div className="searchbar-container">
+                    <Searchbar setResults={setResults} setShowSearch={setShowSearch} />
+                    <SearchResults results={results} setShelfBooks={setShelfBooks} shelfBooks={shelfBooks} />
+                </div>
+            }
+            {!showSearch &&
+                <div className="shelf">
+                    <Shelf setShelfBooks={setShelfBooks} shelfBooks={shelfBooks} setShowSearch={setShowSearch} />
+                </div>
             }
         </div>
     );
